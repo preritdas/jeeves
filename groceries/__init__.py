a5ff2d2af5b567d0a2dac8b20cddce4a12064f3a
Prerit Das <preritdas@gmail.com>
2022-09-25 18:51:01 -0400

    Use dict.get to read optional options.

--- groceries/__init__.py
+++ groceries/__init__.py
@@ -1,9 +1,8 @@
 from . import classification
 
 def handler(content: str, options: dict):
-    # Determine setup
-    if "setup" in options: setup = options["setup"]
-    else: setup = None
+    # Options
+    setup = options.get("setup", None)
 
     return classification.classify_grocery_list(content, setup=setup)
     
