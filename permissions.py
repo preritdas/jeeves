f13578cc3a8f256229613bbd99b44c855546b2ac
Prerit Das <preritdas@gmail.com>
2022-09-25 19:29:22 -0400

    Read permissions from Deta Base.

---
+++ permissions.py
@@ -0,0 +1,45 @@
+"""
+Read persmissions stored in a Deta Base.
+"""
+# External
+import deta
+
+# Project
+import keys
+
+
+deta = deta.Deta(keys.Deta.project_key)
+permissions_db = deta.Base("permissions")
+
+
+def db_init():
+    """
+    If there's nothing in the Base, fire it up with the proper
+    fields and formatting.
+
+    Call this function manually.
+    """
+    permissions_db.put(
+        {
+            "Name": "Prerit Das",
+            "Phone": "14259023246",
+            "Permissions": "all"
+        }
+    )
+
+
+def check_permissions(phone: str, app_name: str) -> bool:
+    items = permissions_db.fetch(query={"Phone": phone}).items
+
+    if not items: return False
+
+    for entry in items:
+        permissions = [ele.strip() for ele in entry["Permissions"].lower().split(",")]
+
+        if "all" in permissions: 
+            return True
+
+        if app_name.lower() in permissions:
+            return True
+
+    return False
