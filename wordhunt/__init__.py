0293763a33a1e0be015b070c5e1b71396c332bde
Prerit Das <preritdas@gmail.com>
2022-09-25 18:49:53 -0400

    Import wordhunt app.

--- wordhunt/__init__.py
+++ wordhunt/__init__.py
@@ -1,6 +1,10 @@
-import main
+from . import wordhunt
 
 
-def handler():
-    pass
-    return "", 204
+def handler(content: str, options: dict):
+    height = options.get("height", 4)
+    width = options.get("width", 4)
+    limit = options.get("limit", 20)
+
+    res = wordhunt.all_possibilities(wordhunt.create_board(content, width, height))
+    return wordhunt.print_results(res, limit)
