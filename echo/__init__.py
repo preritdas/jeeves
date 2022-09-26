e648f551f399bb707891a5278c85da7dd4fc2c34
Prerit Das <preritdas@gmail.com>
2022-09-25 23:38:48 -0400

    Echo app help.

--- echo/__init__.py
+++ echo/__init__.py
@@ -1,8 +1,14 @@
+from re import L
 import texts
 
 
 def handler(content: str, options: dict) -> str:
     """Send texts."""
+    if options.get("help", None):
+        return "Send a message (content) to someone else on my behalf. \n\n" \
+            "Available options:\n" \
+            "- recipient: recipient's phone number"
+
     if not "recipient" in options:
         return "You must provide the recipient's phone number as an option."
     
