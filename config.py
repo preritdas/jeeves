401911d97488416484cd5fc25a1e2fbcece1ccc2
Prerit Das <preritdas@gmail.com>
2022-09-26 18:58:49 -0400

    New config module.

---
+++ config.py
@@ -0,0 +1,16 @@
+"""Read config values."""
+import os
+import configparser
+
+
+config = configparser.RawConfigParser()
+config.read(
+    os.path.join(
+        (current_dir := os.path.dirname(os.path.realpath(__file__))),
+        "config.ini"
+    )
+)
+
+
+class Weather:
+    default_city = config["Weather"]["default_city"]
