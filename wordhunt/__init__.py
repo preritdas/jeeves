8c6d6aea52f670d0c982f46a62f0cfbfc4f9f251
Prerit Das <preritdas@gmail.com>
2022-09-25 23:47:25 -0400

    WordHunt app help.

--- wordhunt/__init__.py
+++ wordhunt/__init__.py
@@ -2,6 +2,13 @@ from . import wordhunt
 
 
 def handler(content: str, options: dict):
+    if options.get("help", None):
+        return "Solve a WordHunt board.\n\n" \
+            "Available options:\n" \
+            "- height: board height, default 4" \
+            "- width: board width, default 4" \
+            "- limit: results limit, default 20"
+
     height = options.get("height", 4)
     width = options.get("width", 4)
     limit = options.get("limit", 20)
