707fa731c50e0c64262435441bb7c759b3964c24
Prerit Das <preritdas@gmail.com>
2022-09-25 23:40:26 -0400

    Grocery app help.

--- groceries/__init__.py
+++ groceries/__init__.py
@@ -1,6 +1,11 @@
 from . import classification
 
 def handler(content: str, options: dict):
+    if options.get("help", None):
+        return "Organize your grocery list by item category.\n\n" \
+            "Available options:\n" \
+            "- setup: custom store setup, ex. whole foods"
+
     # Options
     setup = options.get("setup", None)
 
