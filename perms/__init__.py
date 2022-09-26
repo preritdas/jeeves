61c75b2ee5e5db4a4fa172d670ed4b11849fd186
Prerit Das <preritdas@gmail.com>
2022-09-25 23:46:04 -0400

    Help for permissions app.

--- perms/__init__.py
+++ perms/__init__.py
@@ -1,20 +1,28 @@
 import permissions
 
 
-def handler(content: str, options: dict) -> str:
+def handler(content: str, options: dict[str, str]) -> str:
     """Update permissions in the database."""
     assert (content := content.lower()), "You must provide some content here."
+
+    if options.get("help", None):
+        return "Add permissions for a new user, or update those of a current one.\n\n" \
+            "Message content should be the new permissions.\n\n" \
+            "Available options:\n" \
+            "- action: REQUIRED, either 'create' or 'update'" \
+            "- phone: REQUIRED, phone number of new or existing user" \
+            "- name: MAYBE, only needed if adding a new user."
 
     if options["action"] == "create":
         permissions.permissions_db.put(
             {
-                "Name": options["name"],
+                "Name": options["name"].title(),
                 "Phone": options["phone"],
                 "Permissions": content
             }
         )
         
-        return f"Added permissions for {options['name']}. {content}."
+        return f"Added permissions for {options['name'].title()}. {content}."
 
     if options["action"] == "update":
         db_res = permissions.permissions_db.fetch(
