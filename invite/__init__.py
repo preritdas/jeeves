7eb6a7dae12b7c1cdb2605e9c161e3f3c7de1b47
Prerit Das <preritdas@gmail.com>
2022-09-28 20:45:31 -0400

    Sample options request in invite message.

--- invite/__init__.py
+++ invite/__init__.py
@@ -20,7 +20,8 @@ def handler(content: str, options: dict) -> str:
     invite_content = "Good day, sir. My man has invited you to avail of my services. " \
         "Text me 'app: apps' to see what I can do.\n\n" \
         "To view an app's help, send 'options: help = yes' below your " \
-        "app name statement."
+        "app name statement.\n\n" \
+        "app: jokes\noptions: help = yes"
 
     texts.send_message(invite_content, recipient)
 
