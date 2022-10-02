fdb563b0181854f97d0fdd33c9c36ae0ded309ef
Prerit Das <preritdas@gmail.com>
2022-10-02 16:55:41 -0400

    WordHunt app tests.

---
+++ tests/test_app_wordhunt.py
@@ -0,0 +1,13 @@
+import app_wordhunt
+
+
+def test_handler():
+    res = app_wordhunt.handler(
+        content = "nahzuxtskdyxpaus",
+    )
+
+    assert "thanx" in res
+
+
+def test_help():
+    assert "Solve a" in app_wordhunt.handler("", {"help": "yes"})
