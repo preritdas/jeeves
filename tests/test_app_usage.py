2b55eedafaed21cb1476a9e991862aa9cad23d82
Prerit Das <preritdas@gmail.com>
2022-10-02 16:47:20 -0400

    Usage app tests.

---
+++ tests/test_app_usage.py
@@ -0,0 +1,15 @@
+import app_usage
+
+
+def test_handler():
+    res = app_usage.handler(
+        content = "",
+        options = {}
+    )
+
+    assert "was pinged" in res
+
+
+def test_help():
+    res = app_usage.handler("", {"help": "yes"})
+    assert "usage report" in res
