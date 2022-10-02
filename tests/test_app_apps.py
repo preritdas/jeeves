95e3243177eefa2fd7f07179e4574e4c01b03441
Prerit Das <preritdas@gmail.com>
2022-10-02 15:46:01 -0400

    Tests for app_apps.

---
+++ tests/test_app_apps.py
@@ -0,0 +1,10 @@
+import app_apps
+
+
+def test_handler():
+    assert "The following apps are available" in app_apps.handler("", {})
+
+
+def test_program_list():
+    assert type(app_apps.PROGRAMS) is dict and \
+        all([key for key in app_apps.PROGRAMS if isinstance(key, str)])
