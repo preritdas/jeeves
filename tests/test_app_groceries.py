63665818485822d2ffc4dc6e55c330c11698f4de
Prerit Das <preritdas@gmail.com>
2022-10-02 16:07:11 -0400

    Groceries app tests.

---
+++ tests/test_app_groceries.py
@@ -0,0 +1,20 @@
+import app_groceries
+
+
+def test_handler():
+    res = app_groceries.handler(
+        content = "Apples\nBananas",
+        options = {"setup": "whole foods"}
+    )
+
+    assert "List ID" in res
+    assert "Apples" in res
+
+
+def test_help():
+    res = app_groceries.handler(
+        content = "",
+        options = {"help": "yes"}
+    )
+
+    assert "setup" in res and "grocery list" in res
