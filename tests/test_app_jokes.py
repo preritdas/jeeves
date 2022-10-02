aacbf1afc76751b388a58ae92e2d9b5c42cd8ecb
Prerit Das <preritdas@gmail.com>
2022-10-02 16:17:28 -0400

    Jokes app help.

---
+++ tests/test_app_jokes.py
@@ -0,0 +1,10 @@
+import app_jokes
+
+
+def test_handler():
+    assert app_jokes.handler("", options={"tags": "nsfw,dark"})
+
+
+def test_help():
+    res = app_jokes.handler("", options={"help": "yes"})
+    assert "random joke" in res and "separated categories" in res
