8af895dc7a1e2527cc689352c6ea2dbb82749776
Prerit Das <preritdas@gmail.com>
2022-10-02 15:53:32 -0400

    Test the help.

--- tests/test_app_apps.py
+++ tests/test_app_apps.py
@@ -8,3 +8,7 @@ def test_handler():
 def test_program_list():
     assert type(app_apps.PROGRAMS) is dict and \
         all([key for key in app_apps.PROGRAMS if isinstance(key, str)])
+
+
+def test_help():
+    assert "available apps" in app_apps.handler(content="", options={"help": "yes"})
