44ab3bc39d252f32e64310968cadecd44b6c74dc
Prerit Das <preritdas@gmail.com>
2022-10-10 18:27:13 -0400

    Ability to query the last list of a user.
    Also, no more "id" option. The id is supplied in the "add" option, either "last" or a raw List ID.

--- app_groceries/__init__.py
+++ app_groceries/__init__.py
@@ -1,4 +1,8 @@
+"""Groceries app."""
+import datetime as dt
+
 import utils
+import config
 
 from . import classification
 from permissions import deta
@@ -15,17 +19,34 @@ APP_OPTIONS = {
 grocery_db = deta.Base("groceries")
 
 
+def latest_grocery_list(phone: str) -> dict:
+    """Finds the last grocery list belonging to `phone`. Returns an empty dictionary if there are no lists belonging to `phone`."""
+    db_res = grocery_db.fetch({"phone": phone}).items
+    
+    if not db_res: 
+        return {}
+
+    return max(
+        db_res, 
+        key = lambda item: dt.datetime.strptime(
+            item["time"], 
+            config.Groceries.FULL_DT_FORMAT
+        )
+    )
+    
+
 @utils.app_handler(APP_HELP, APP_OPTIONS)
 def handler(content: str, options: dict):
     # Options
     setup = options.get("setup", None)
 
-    if options.get("add", None):
-        old_key = options["id"]
-        old_list = grocery_db.get(old_key)
+    if (list_id := options.get("add", None)):
+        # Get the user's last list
+        old_list = latest_grocery_list(options["inbound_phone"]) if list_id == "last" \
+            else grocery_db.get(list_id)
 
         if not old_list:
-            return f"No existing list was found with ID {options['id']}."
+            return f"No existing list was found."
 
         # Append old list to current list 
         old_list = old_list["list"]
@@ -35,7 +56,13 @@ def handler(content: str, options: dict):
     sorted_list = classification.classify_grocery_list(content, setup=setup)
 
     # IDs
-    key = grocery_db.put({"list": content})["key"]
+    put_item = grocery_db.put(
+        {
+            "list": content,
+            "phone": options["inbound_phone"],
+            "time": dt.datetime.now().strftime(config.Groceries.FULL_DT_FORMAT)
+        }
+    )
     
-    return f"List ID: {key}\n\n{sorted_list}"
+    return f"List ID: {put_item['key']}\n\n{sorted_list}"
     
