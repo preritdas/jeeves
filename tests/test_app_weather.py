77ee59c48ad6b4d18277fe18a47cd3768c5275f9
Prerit Das <preritdas@gmail.com>
2022-10-10 22:48:39 -0400

    Test weather app data funcs.

--- tests/test_app_weather.py
+++ tests/test_app_weather.py
@@ -20,3 +20,23 @@ def test_handler():
 
 def test_help():
     assert "weather data" in app_weather.handler("", {"help": "yes"})
+
+
+def test_data_invalid_info():
+    """
+    Test invalid state and country codes in the app_weather.data.current_weather 
+    function.
+    """
+    res = app_weather.handler("", {"inbound_phone": "12223334455", "state": "lol"})
+    assert "State must be" in res
+
+    res = app_weather.handler("", {"inbound_phone": "12223334455", "country": "lol"})
+    assert "Country must be" in res
+
+
+def test_invalid_city():
+    CITY = "notacity"
+
+    res = app_weather.handler("", {"inbound_phone": "12223334455", "city": CITY})
+    assert "was not found" in res
+    assert CITY in res  # make sure the bad input is sent back to user
