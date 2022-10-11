31b4d38d51148983901354ad71f403e719027fb8
Prerit Das <preritdas@gmail.com>
2022-10-10 22:38:05 -0400

    Test nuances in permission checking.

--- tests/test_permissions.py
+++ tests/test_permissions.py
@@ -1,7 +1,26 @@
+"""
+Check permissions. 
+
+12223334455 is a custom testing number used in the permissions
+database. It has permissions to use _all_ apps. 
+
+00000000000 is a custom testing number
+in the permissions database. It has permissions to use _only_ the groceries app.
+"""
 import permissions
 
 
 def test_check_permissions():
     assert permissions.check_permissions(
         "12223334455", "wordhunt"
+    )
+
+
+def test_custom_permissions():
+    assert not permissions.check_permissions(
+        "00000000000", "wordhunt"
+    )
+
+    assert permissions.check_permissions(
+        "00000000000", "groceries"
     )
