8617e24521cae42b33b6cd3f3739753889b389f7
Prerit Das <preritdas@gmail.com>
2022-10-12 22:48:50 -0400

    Skip the wordhunt app handler test for now.
    Same Vonage inbound-sms timeout error.

--- tests/test_app_wordhunt.py
+++ tests/test_app_wordhunt.py
@@ -1,6 +1,10 @@
+"""Test the WordHunt app."""
+import pytest
+
 import app_wordhunt
 
 
+@pytest.mark.skip(reason="Inbound SMS Vonage timeout error, app currently disabled.")
 def test_handler():
     res = app_wordhunt.handler(
         content = "nahzuxtskdyxpaus",
