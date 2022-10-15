afb17418c338dc0219b9d1b4fd39019fe586b8fd
Prerit Das <preritdas@gmail.com>
2022-10-15 01:46:10 -0400

    Basic app working - return string formatted `Drink`.

--- app_cocktails/__init__.py
+++ app_cocktails/__init__.py
@@ -1,6 +1,8 @@
 """Get cocktail drink recommendations with ingredients and instructions."""
 import utils
 
+from . import data
+
 
 APP_HELP = "Get cocktail drink recommendations with ingredients and instructions."
 APP_OPTIONS = {}
@@ -8,4 +10,6 @@ APP_OPTIONS = {}
 
 @utils.app_handler(APP_HELP, APP_OPTIONS)
 def handler(content: str, options: dict) -> str:
-    return "Cocktail."
+    """Gather and report the drink info. String format it."""
+    drink = data.random_cocktail()
+    return str(drink)
