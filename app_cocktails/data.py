3c07a8ba27051ba121fe17b8a6f04f5786fdb512
Prerit Das <preritdas@gmail.com>
2022-10-15 01:37:22 -0400

    Gather drink ingredients, amounts, and instructions.

--- app_cocktails/data.py
+++ app_cocktails/data.py
@@ -7,7 +7,16 @@ ENDPOINT = "https://www.thecocktaildb.com/api/json/v1/1/"
 
 def _parse_ingredients(raw_drink: dict) -> dict:
     """Returns a dictionary mapping between ingredient names and their quantities."""
+    all_ingredients = {}
+
+    for n_ingredient in range(1, 16):
+        if not raw_drink[(loc := f"strIngredient{n_ingredient}")]:
+            break
     
+        all_ingredients[raw_drink[loc].strip()] = \
+            raw_drink[f"strMeasure{n_ingredient}"].strip()
+
+    return all_ingredients
 
 
 def random_cocktail() -> dict:
@@ -19,15 +28,10 @@ def random_cocktail() -> dict:
         return random_cocktail()
 
     raw_drink = response["drinks"][0]
-    drink_attrs = {"name": raw_drink["strDrink"]}
-    
-    return raw_drink
+    drink_attrs = {
+        "name": raw_drink["strDrink"],
+        "ingredients": _parse_ingredients(raw_drink),
+        "instructions": raw_drink["strInstructions"]
+    }
 
-
-
-
-
-
-
-if __name__ == '__main__':
-    print(random_cocktail())
+    return drink_attrs
