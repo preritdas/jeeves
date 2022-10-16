a3a710020f29450d269305ad386055407025ecfd
Prerit Das <preritdas@gmail.com>
2022-10-15 23:51:46 -0400

    Tests for cocktail querying.

--- tests/test_app_cocktails.py
+++ tests/test_app_cocktails.py
@@ -9,6 +9,23 @@ def test_handler():
     assert "behold" in res.lower() and "enjoy" in res.lower()
 
 
+def test_querying():
+    res = app_cocktails.handler("tequila", {"inbound_phone": "12223334455"})
+
+    # Assert that the right drinks were found
+    res_test = res.lower()
+
+    assert "tequila fizz" in res_test
+    assert "tequila sour" in res_test
+
+
+def test_no_query_found():
+    res = app_cocktails.handler("lmfao", {"inbound_phone": "12223334455"})
+
+    # Assert no drink found messages
+    assert "no drink" in res.lower()
+
+
 def test_help():
     help_text = app_cocktails.handler(
         "", 
