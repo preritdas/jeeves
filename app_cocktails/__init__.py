5f764159924d1ba6af39992f32445a1b87e0517b
Prerit Das <preritdas@gmail.com>
2022-10-15 23:48:06 -0400

    Implement ability to query drinks.

--- app_cocktails/__init__.py
+++ app_cocktails/__init__.py
@@ -4,12 +4,24 @@ import utils
 from . import data
 
 
-APP_HELP = "Get cocktail drink recommendations with ingredients and instructions."
+APP_HELP = (
+    "Get cocktail drink recommendations with ingredients and instructions. "
+    "You can search for a drink by providing it as content. "
+    "The top three results are sent back."
+)
 APP_OPTIONS = {}
 
 
 @utils.app_handler(APP_HELP, APP_OPTIONS)
 def handler(content: str, options: dict) -> str:
     """Gather and report the drink info. String format it."""
+    if content:  # searching for a drink
+        drinks = data.search_cocktails(content)
+
+        if not drinks:
+            return f"No drink was found with the name {content}."
+
+        return data.concat_drinks(drinks, limit=3)
+
     drink = data.random_cocktail()
     return str(drink)
