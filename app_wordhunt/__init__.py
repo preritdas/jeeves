6c8852ec6451274971d85b78e754b220d5e34419
Prerit Das <preritdas@gmail.com>
2022-10-20 15:03:09 -0400

    Correct parameter typing and length checking.
    If height and width are provided as option, they should be read in as ints. If a board is sent in with a size that doesn't match the provided/default height and width, inform the user.

--- app_wordhunt/__init__.py
+++ app_wordhunt/__init__.py
@@ -17,9 +17,13 @@ def handler(content: str, options: dict) -> str:
     if not content:
         return "You must provide the board layout as content."
 
-    height = options.get("height", 4)
-    width = options.get("width", 4)
-    limit = options.get("limit", 20)
+    height = int(options.get("height", 4))
+    width = int(options.get("width", 4))
+    limit = int(options.get("limit", 20))
+
+    if len(content) != height * width:
+        return f"The board is {height} tall and {width} wide, but you provided a " \
+            f"{len(content)} long board."
 
     res = wordhunt.all_possibilities(wordhunt.create_board(content, width, height))
     return wordhunt.print_results(res, limit)
