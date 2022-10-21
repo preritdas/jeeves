76274dcf05e25dd972632a414ebef133f5fceffa
Prerit Das <preritdas@gmail.com>
2022-10-21 17:20:07 -0400

    Test failure cases.

--- tests/test_app_wordhunt.py
+++ tests/test_app_wordhunt.py
@@ -14,5 +14,23 @@ def test_handler():
     assert "thanx" in res
 
 
+def test_no_content():
+    res = app_wordhunt.handler(
+        content = "",
+        options = {}
+    )
+
+    assert "You must provide" in res
+
+
+def test_bad_dimensions():
+    res = app_wordhunt.handler(
+        content = "a", 
+        options = {}
+    )
+
+    assert "The board is" in res
+
+
 def test_help():
     assert "Solve a" in app_wordhunt.handler("", {"help": "yes"})
