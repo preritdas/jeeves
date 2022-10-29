84944c90cbddfe60f7431faaa7414f7a5873c2af
Prerit Das <preritdas@gmail.com>
2022-10-29 13:34:32 -0400

    Test Vonage account balance > $3.

---
+++ tests/test_vonage.py
@@ -0,0 +1,8 @@
+"""Check Vonage balance, account status, etc."""
+import texts
+
+
+def test_vonage_balance():
+    """Make sure the Vonage account has at least $3 loaded."""
+    balance = float(texts.nexmo_client.get_balance()["value"])
+    assert balance > 3
