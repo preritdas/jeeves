6f1dce5273118be4de013da31f011c0b79db2369
Prerit Das <preritdas@gmail.com>
2022-10-29 20:23:37 -0400

    Test extended features in the wordhunt app.
    The module still has to be rewritten, potentially compiled in Cython, because it's extremely slow.

--- tests/test_app_wordhunt.py
+++ tests/test_app_wordhunt.py
@@ -1,7 +1,22 @@
 """Test the WordHunt app."""
+# External
 import pytest
 
+# Internal
+import random
+import string
+
+# Project
 import app_wordhunt
+
+
+@pytest.fixture
+def random_board():
+    return app_wordhunt.wordhunt.Board.from_letters(
+        letters = "".join(random.choices(string.ascii_lowercase, k=16)), 
+        width = 4, 
+        height = 4
+    )
 
 
 # @pytest.mark.skip(reason="Inbound SMS Vonage timeout error, app currently disabled.")
@@ -30,6 +45,20 @@ def test_bad_dimensions():
     )
 
     assert "The board is" in res
+
+
+def test_string_board(random_board):
+    assert "\n" in str(random_board)
+
+
+def test_value_error(random_board):
+    with pytest.raises(ValueError):
+        app_wordhunt.wordhunt.Board.from_letters("asd", 4, 4)
+
+
+def test_print_results_no_limit(random_board):
+    possibilities = app_wordhunt.wordhunt.all_possibilities(random_board)
+    assert type(app_wordhunt.wordhunt.print_results(possibilities)) is str
 
 
 def test_help():
