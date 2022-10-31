4a2d462512c10942475ce6dbe06954d580df1fc4
Prerit Das <preritdas@gmail.com>
2022-10-31 16:43:22 -0400

    Utilize new structure of `inbound.main_handler`.

--- tests/test_main_handler.py
+++ tests/test_main_handler.py
@@ -2,15 +2,35 @@
 import inbound
 
 
-def test_sandbox(mocker):
+def test_success(mocker, default_inbound):
     mocker.patch("inbound.texts.config.General.SANDBOX_MODE", True)
+    mocker.patch("inbound.usage.config.General.SANDBOX_MODE", True)
 
-    content, status_code = inbound.main_handler(
+    res = inbound.main_handler(
         {
-            "msisdn": "14259023246",
-            "text": "app: apps"
+            **default_inbound,
         }
     )
 
-    assert not content
-    assert 200 <= status_code < 300
+    assert res["response"]
+    assert not res["http"][0]
+    assert 200 <= res["http"][1] < 300
+
+
+def test_message_concat(mocker, default_inbound):
+    """If the message is too long."""
+    mocker.patch("inbound.texts.config.General.SANDBOX_MODE", True)
+    mocker.patch("inbound.usage.config.General.SANDBOX_MODE", True)
+
+    res = inbound.main_handler(
+        {
+            **default_inbound,
+            "text": "test message that is too long and is split by the carrier",
+            "concat": "true",
+            "concat-part": "1"
+        }
+    )
+
+    assert "Your message was too long" in res["response"]
+    assert not res["http"][0]
+    assert 200 <= res["http"][1] < 300
