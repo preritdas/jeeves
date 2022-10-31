bde494399a6fbb550c1effc1d4b8e32880df27df
Prerit Das <preritdas@gmail.com>
2022-10-31 16:57:55 -0400

    Test remaining handler response cases.

--- tests/test_main_handler.py
+++ tests/test_main_handler.py
@@ -2,7 +2,7 @@
 import inbound
 
 
-def test_success(mocker, default_inbound):
+def test_no_permissions(mocker, default_inbound):
     mocker.patch("inbound.texts.config.General.SANDBOX_MODE", True)
     mocker.patch("inbound.usage.config.General.SANDBOX_MODE", True)
 
@@ -12,7 +12,7 @@ def test_success(mocker, default_inbound):
         }
     )
 
-    assert res["response"]
+    assert "you don't have permission" in res["response"]
     assert not res["http"][0]
     assert 200 <= res["http"][1] < 300
 
@@ -32,5 +32,71 @@ def test_message_concat(mocker, default_inbound):
     )
 
     assert "Your message was too long" in res["response"]
+    assert not res["http"][0]
+    assert 200 <= res["http"][1] < 300
+
+
+def test_invalid_inbound(mocker, default_inbound):
+    mocker.patch("inbound.texts.config.General.SANDBOX_MODE", True)
+    mocker.patch("inbound.usage.config.General.SANDBOX_MODE", True)
+
+    res = inbound.main_handler(
+        {
+            **default_inbound,
+            "text": "hello",
+        }
+    )
+
+    assert "invalid" in res["response"]
+    assert not res["http"][0]
+    assert 200 <= res["http"][1] < 300
+
+
+def test_invalid_app(mocker, default_inbound):
+    mocker.patch("inbound.texts.config.General.SANDBOX_MODE", True)
+    mocker.patch("inbound.usage.config.General.SANDBOX_MODE", True)
+
+    res = inbound.main_handler(
+        {
+            **default_inbound,
+            "text": "app: askjdcasldjc",
+        }
+    )
+
+    assert "app does not exist" in res["response"]
+    assert not res["http"][0]
+    assert 200 <= res["http"][1] < 300
+
+
+def test_run_app(mocker, user_git_pytest):
+    mocker.patch("inbound.texts.config.General.SANDBOX_MODE", True)
+    mocker.patch("inbound.usage.config.General.SANDBOX_MODE", True)
+
+    res = inbound.main_handler(
+        {
+            "msisdn": user_git_pytest["Phone"],
+            "text": "app: apps",
+        }
+    )
+
+    assert "available" in res["response"]
+    assert not res["http"][0]
+    assert 200 <= res["http"][1] < 300
+
+
+def test_app_error_handling(mocker, user_git_pytest):
+    mocker.patch("inbound.texts.config.General.SANDBOX_MODE", True)
+    mocker.patch("inbound.usage.config.General.SANDBOX_MODE", True)
+
+    res = inbound.main_handler(
+        {
+            "msisdn": user_git_pytest["Phone"],
+            "text": "app: wordhunt\n" \
+                "options: width = notnumber; height = notnumber\n" \
+                "hagsyausidmsnajs"
+        }
+    )
+
+    assert "Unfortunately" in res["response"]
     assert not res["http"][0]
     assert 200 <= res["http"][1] < 300
