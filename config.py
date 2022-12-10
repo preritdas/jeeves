aa6e70910f2e764780768ee4f4e7260d4ca31e6e
Prerit Das <preritdas@gmail.com>
2022-12-10 17:13:58 -0500

    New config design with boolean reading and custom exceptions.

--- config.py
+++ config.py
@@ -27,9 +27,34 @@ config.read(
 )
 
 
+class ConfigurationError(Exception):
+    """Raise this error if an invalid configuration value is provided."""
+    def __init__(self, value, config_path: str = ""):
+        message = ""
+        if config_path:
+            message += f"An invalid configuration was provided for {config_path}. "
+
+        message += f"You provided {value}, which is invalid."
+        super().__init__(message)
+
+
+def read_bool_option(option: str) -> bool:
+    """
+    Read a boolean preference, ex. true/yes/no, and turn it into a boolean for 
+    a config class. Meant to read items from a configuration file that only reads
+    text string value.
+    """
+    if option.lower() in {"true, yes, on"}:
+        return True
+    elif option.lower() in {"false, no, off"}:
+        return False
+    else:
+        raise ConfigurationError(option)
+
+
 class General:
-    SANDBOX_MODE: bool = True if config["General"]["sandbox_mode"].lower() == "true" else False
-    THREADED_INBOUND: bool = True if config["General"]["threaded_inbound"].lower() == "true" else False
+    SANDBOX_MODE: bool = read_bool_option(config["General"]["sandbox_mode"])
+    THREADED_INBOUND: bool = read_bool_option(config["General"]["threaded_inbound"])
 
 
 class Weather:
@@ -37,9 +62,7 @@ class Weather:
 
 
 class Groceries:
-    TRANSLATION: bool = True if config["Groceries"]["translation"].lower().strip() == "true" \
-        else False
-
+    THREADED_INBOUND: bool = read_bool_option(config["Groceries"]["translation"])
     FULL_DT_FORMAT: str = "%Y-%m-%d %H-%M-%S"
 
 
