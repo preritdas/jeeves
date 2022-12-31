34c9b13cd3718bd4b54c1287a1d7ddadb5e0b1c6
Prerit Das <preritdas@gmail.com>
2022-12-31 13:44:04 -0500

    Include options arg and set it low.

--- tests/test_app_gpt.py
+++ tests/test_app_gpt.py
@@ -5,7 +5,8 @@ from apps import gpt
 def test_handler():
     """Test the GPT applet handler."""
     res = gpt.handler(
-        content = "Write a short three-word tweet."
+        content = "Write a short three-word tweet.",
+        options = {"tokens": "5"}
     )
 
     assert res
