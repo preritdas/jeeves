88d7be4b5507a2224aa238252911cbfff095e97e
Prerit Das <preritdas@gmail.com>
2023-01-07 17:33:14 -0500

    Test threaded and non threaded root API responses.

--- tests/test_main_app.py
+++ tests/test_main_app.py
@@ -1,12 +1,12 @@
 from fastapi.testclient import TestClient
 import pytest
 
-from main import app
+import main
 
 
 @pytest.fixture(scope="module")
 def test_client():
-    return TestClient(app=app)
+    return TestClient(app=main.app)
 
 
 def test_testing_endpoint(test_client):
@@ -14,3 +14,29 @@ def test_testing_endpoint(test_client):
 
     assert "All working here" in res.text
     assert res.status_code == 200
+
+
+def test_apps_non_threaded(test_client, default_inbound, mocker):
+    mocker.patch("main.inbound.texts.config.General.SANDBOX_MODE", True)
+    mocker.patch("main.inbound.usage.config.General.SANDBOX_MODE", True)
+
+    mocker.patch("main.config.General.THREADED_INBOUND", False)
+
+    json = {**default_inbound}
+    del json["concat"]
+
+    res = test_client.post("/inbound-sms", json=json)
+    assert res.status_code == 204
+
+
+def test_apps_threaded(test_client, default_inbound, mocker):
+    mocker.patch("main.inbound.texts.config.General.SANDBOX_MODE", True)
+    mocker.patch("main.inbound.usage.config.General.SANDBOX_MODE", True)
+
+    mocker.patch("main.config.General.THREADED_INBOUND", True)
+
+    json = {**default_inbound}
+    del json["concat"]
+
+    res = test_client.post("/inbound-sms", json=json)
+    assert res.status_code == 204
