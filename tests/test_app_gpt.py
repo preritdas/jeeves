44c75159287b8d098c35ef9a03b2f2654ef1503a
Prerit Das <preritdas@gmail.com>
2023-01-07 17:58:32 -0500

    Update the GPT prompt in test.

--- tests/test_app_gpt.py
+++ tests/test_app_gpt.py
@@ -5,8 +5,8 @@ from apps import gpt
 def test_handler():
     """Test the GPT applet handler."""
     res = gpt.handler(
-        content = "Write a short three-word tweet.",
-        options = {"tokens": "5"}
+        content = "Give me three rhyming words.",
+        options = {"tokens": "15"}
     )
 
     assert res
