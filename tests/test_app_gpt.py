cd35fb7b0a6e8d4f9a5edbe679928024546cb86a
Prerit Das <preritdas@gmail.com>
2023-03-01 21:08:26 -0500

    No tokens option.

--- tests/test_app_gpt.py
+++ tests/test_app_gpt.py
@@ -8,8 +8,7 @@ from apps import gpt
 def test_handler():
     """Test the GPT applet handler."""
     res = gpt.handler(
-        content = "Give me three short rhyming words.",
-        options = {"tokens": "20"}
+        content = "Give me three short rhyming words."
     )
 
     assert res
