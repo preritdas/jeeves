d6d5e8c787d9fa0209eff9862740d673fcbad551
Prerit Das <preritdas@gmail.com>
2023-03-01 21:10:56 -0500

    No app options.

--- apps/gpt/__init__.py
+++ apps/gpt/__init__.py
@@ -7,7 +7,7 @@ from . import completions
 APP_HELP = "Get a GPT response."
 
 
-@utils.app_handler(APP_HELP, APP_OPTIONS)
+@utils.app_handler(APP_HELP)
 def handler(content: str, options: dict[str, str]) -> str:
     """Handler for the GPT applet."""
     response: str = completions.gpt_response(content)
