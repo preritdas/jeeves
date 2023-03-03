7aee3cc9348f5eb2736a2d6f63e4916aaa27d9c4
Prerit Das <preritdas@gmail.com>
2023-03-02 21:05:53 -0500

    Lower Vonage required balance to $1.

--- tests/test_vonage.py
+++ tests/test_vonage.py
@@ -5,4 +5,4 @@ import texts
 def test_vonage_balance():
     """Make sure the Vonage account has at least $3 loaded."""
     balance = float(texts.nexmo_client.get_balance()["value"])
-    assert balance > 2
+    assert balance > 1
