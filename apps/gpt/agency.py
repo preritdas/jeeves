430b80b0050734057444e9fea6d3de9dea28fcbe
Prerit Das <preritdas@gmail.com>
2023-04-17 00:21:51 +0000

    Retry agent if couldn't parse.

--- apps/gpt/agency.py
+++ apps/gpt/agency.py
@@ -18,6 +18,20 @@ llm = ChatOpenAI(model_name="gpt-4", openai_api_key=keys.OpenAI.API_KEY, tempera
 agent = initialize_agent(toolkit, llm)
 
 
+def retry_couldnt_parse(function):
+    """Decorator to retry three times if the agent couldn't parse the output."""
+    def wrapper(*args, **kwargs):
+        for _ in range(3):
+            try:
+                return function(*args, **kwargs)
+            except ValueError as e:
+                if "Could not parse LLM output" in str(e):
+                    continue
+                raise e
+    return wrapper
+
+
+@retry_couldnt_parse
 def run_agent(query: str) -> str:
     """Run the agent."""
-    return agent.run(query)
+    agent.run(query)
