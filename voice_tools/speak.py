96225cd3ddaf2ecc37b05fad3ea46696e67b7d31
Prerit Das <preritdas@gmail.com>
2023-04-20 19:35:22 +0000

    Speak with Jeeves and write to file.

--- voice_tools/speak.py
+++ voice_tools/speak.py
@@ -1,13 +1,27 @@
 """Use ElevenLabs to speak."""
 import elevenlabs
 
+import os
+import uuid
+
 from keys import KEYS
+
+
+# Voice cache folder
+if not os.path.exists("voice_cache"):
+    os.mkdir("voice_cache")
 
 
 JEEVES_VOICE_ID = "O1lUlMPuT6IOj4I43c6s"
 
 
-def speak_jeeves(text: str) -> bytes:
-    """Speak the text using the Jeeves voice."""
-    return elevenlabs.generate(text, KEYS["ElevenLabs"]["api_key"], JEEVES_VOICE_ID)
+def speak_jeeves(text: str) -> str:
+    """Speak the text using the Jeeves voice. Returns a path to the audio file."""
+    byte_code = elevenlabs.generate(text, KEYS["ElevenLabs"]["api_key"], JEEVES_VOICE_ID)
+    filepath = f"{uuid.uuid1()}.mp3"
+
+    with open(f"voice_cache/{filepath}", "wb") as f:
+        f.write(byte_code)
+
+    return filepath
     
