3a3af7576345ade2536d623c1ae9828243247b70
Prerit Das <preritdas@gmail.com>
2023-04-20 20:51:29 +0000

    Use UploadIO to host the audio files.

--- voice_tools/speak.py
+++ voice_tools/speak.py
@@ -1,5 +1,6 @@
 """Use ElevenLabs to speak."""
 import elevenlabs
+import requests
 
 import os
 import uuid
@@ -7,21 +8,34 @@ import uuid
 from keys import KEYS
 
 
-# Voice cache folder
-if not os.path.exists("voice_cache"):
-    os.mkdir("voice_cache")
-
-
 JEEVES_VOICE_ID = KEYS["ElevenLabs"]["voice_id"]
 
 
+def _upload_result(filepath: str) -> str:
+    """Upload the result to the server and return the URL."""
+    url = f"https://api.upload.io/v2/accounts/{KEYS['UploadIO']['account']}/uploads/binary"
+    headers = {
+        "Authorization": f"Bearer {KEYS['UploadIO']['api_key']}",
+        "Content-Type": "audio/mpeg"
+    }
+
+    with open(filepath, "rb") as f:
+        data = f.read()
+        response = requests.post(url, headers=headers, data=data)
+
+    return response.json()["fileUrl"]
+
+
 def speak_jeeves(text: str) -> str:
-    """Speak the text using the Jeeves voice. Returns a path to the audio file."""
+    """Speak the text using the Jeeves voice. Returns a public URL path."""
     byte_code = elevenlabs.generate(text, KEYS["ElevenLabs"]["api_key"], JEEVES_VOICE_ID)
     filepath = f"{uuid.uuid1()}.mp3"
 
-    with open(f"voice_cache/{filepath}", "wb") as f:
+    with open(filepath, "wb") as f:
         f.write(byte_code)
 
-    return filepath
+    public_url = _upload_result(filepath)
+    os.remove(filepath)
+
+    return public_url
     
