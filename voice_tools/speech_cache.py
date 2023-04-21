d3bfab7d05dc0123c15979eb99ee5641dca8d043
Prerit Das <preritdas@gmail.com>
2023-04-21 05:42:35 +0000

    Cache speech and get speech from db.

---
+++ voice_tools/speech_cache.py
@@ -0,0 +1,34 @@
+"""Cache spoken text audio urls."""
+import deta
+
+from keys import KEYS
+
+
+deta_client = deta.Deta(KEYS["Deta"]["project_key"])
+speech_db = deta_client.Base("permissions")
+
+
+def cache_speech(text: str, url: str) -> None:
+    """Cache the speech."""
+    speech_db.put(
+        {
+            "Voice": KEYS["ElevenLabs"]["voice_id"],
+            "Text": text,
+            "URL": url
+        }
+    )
+
+
+def get_speech(text: str) -> str:
+    """Get the speech URL from the cache."""
+    items = speech_db.fetch(
+        query={
+            "Text": text,
+            "Voice": KEYS["ElevenLabs"]["voice_id"]
+        }
+    ).items
+
+    if not items:
+        return ""
+
+    return items[0]["URL"]
