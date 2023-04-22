27fed27dd3fc9a71681b1dce81198893d1a0822d
Prerit Das <preritdas@gmail.com>
2023-04-22 19:56:53 +0000

    Skip old boolean option test.

--- tests/test_config.py
+++ tests/test_config.py
@@ -4,6 +4,7 @@ import pytest
 import config
 
 
+@pytest.mark.skip  # skip as moved to yaml format
 def test_invalid_bool():
     with pytest.raises(config.ConfigurationError):
         config.read_bool_option("asdfaskdlf")
