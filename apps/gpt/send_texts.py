03a1ef0a85ebf48069697752fd79aa5bd71b15e6
Prerit Das <preritdas@gmail.com>
2023-04-23 06:24:16 +0000

    Function to create the tool given `inbound_phone`.

--- apps/gpt/send_texts.py
+++ apps/gpt/send_texts.py
@@ -7,47 +7,56 @@ from typing import Any, Coroutine
 import texts
 
 
-class TextMessageTool(BaseTool):
-    """Wrapper around texts to work best as an agent."""
-    name: str = "Send Text Message"
-    description=(
-        "Useful for when you need to send a text message. Input must be a JSON string with "
-        "the keys \"content\" and \"recipient\" (10-digit phone number preceded by "
-        "country code, ex. \"12223334455\"."
-    )
+def create_text_message_tool(inbound_phone: str) -> BaseTool:
+    """
+    Create a tool to send text messages.
 
-    def __init__(self, inbound_phone: str) -> None:
-        """Initialize."""
-        self.inbound_phone = str(inbound_phone)
+    Args:
+        inbound_phone: The phone number to send a confirmation text to after
+            sending the message.
 
-    def _run(self, query: str) -> str:
-        """Send a text message."""
-        input_parsed = json.loads(query)
+    Returns:
+        A tool to send text messages.
+    """
+    class TextMessageTool(BaseTool):
+        """Wrapper around texts to work best as an agent."""
+        name: str = "Send Text Message"
+        description=(
+            "Useful for when you need to send a text message. Input must be a JSON string with "
+            "the keys \"content\" and \"recipient\" (10-digit phone number preceded by "
+            "country code, ex. \"12223334455\"."
+        )
 
-        # Validate
-        assert "content" in input_parsed, "Input must have a \"content\" key."
-        assert isinstance(input_parsed["content"], str), "Content must be a string."
-        content = input_parsed["content"]
+        def _run(self, query: str) -> str:
+            """Send a text message."""
+            input_parsed = json.loads(query)
 
-        assert "recipient" in input_parsed, "Input must have a \"recipient\" key."
-        assert len(input_parsed["recipient"]) == 11, \
-            "Recipient must be a phone number preceded by country code."
-        recipient = str(input_parsed["recipient"])
+            # Validate
+            assert "content" in input_parsed, "Input must have a \"content\" key."
+            assert isinstance(input_parsed["content"], str), "Content must be a string."
+            content = input_parsed["content"]
 
-        try:
-            send_res = texts.send_message(content=content, recipient=recipient)
-        except Exception as e:
-            return f"Error: {str(e)}"
-        else:
-            texts.send_message(
-                content=(
-                    "Sir, I'm informing you that I have sent the following message to ",
-                    f"{recipient}:\n\n{content}."
-                ),
-                recipient=self.inbound_phone,
-            )
+            assert "recipient" in input_parsed, "Input must have a \"recipient\" key."
+            assert len(input_parsed["recipient"]) == 11, \
+                "Recipient must be a phone number preceded by country code."
+            recipient = str(input_parsed["recipient"])
 
-        return send_res
+            try:
+                send_res = texts.send_message(content=content, recipient=recipient)
+            except Exception as e:
+                return f"Error: {str(e)}"
+            else:
+                texts.send_message(
+                    content=(
+                        "Sir, I'm informing you that I have sent the following message to ",
+                        f"{recipient}:\n\n{content}."
+                    ),
+                    recipient=inbound_phone,
+                )
 
-    def _arun(self, *args: Any, **kwargs: Any) -> Coroutine[Any, Any, str]:
-        raise NotImplementedError(f"{type(self).__name__} does not support async.")
+            return send_res
+
+        def _arun(self, *args: Any, **kwargs: Any) -> Coroutine[Any, Any, str]:
+            raise NotImplementedError(f"{type(self).__name__} does not support async.")
+
+    return TextMessageTool
--- apps/gpt/tool_auth.py
+++ apps/gpt/tool_auth.py
@@ -90,8 +90,7 @@ no_auth_tools = [
             "Input must be a string with the category name. Category must be one of "
             f"{news.MANUAL_AVAILABLE_CATEGORIES}."
         )
-    ),
-    send_texts.TextMessageTool(),
+    )
 ]
 
 
@@ -105,5 +104,9 @@ def build_tools(inbound_phone: str) -> list[Tool]:
         zapier_wrapper = ZapierNLAWrapper(zapier_nla_api_key=zapier_key)
         zapier_toolkit = ZapierToolkit.from_zapier_nla_wrapper(zapier_wrapper)
         added_tools.extend(zapier_toolkit.get_tools())
+
+    # Text messages
+    TextToolClass = send_texts.create_text_message_tool(inbound_phone)
+    added_tools.append(TextToolClass())
 
     return no_auth_tools + added_tools
