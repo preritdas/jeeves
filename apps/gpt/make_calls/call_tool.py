209d9e6dbeba83067863f7c49cd5399955b8823a
Prerit Das <preritdas@gmail.com>
2023-04-25 20:14:08 +0000

    Basic tool built out.

--- apps/gpt/make_calls/call_tool.py
+++ apps/gpt/make_calls/call_tool.py
@@ -1,7 +1,14 @@
 """The calling tool used by agent."""
 from langchain.agents.tools import BaseTool
 
+import json
+from urllib.parse import urlencode
 from typing import Any, Coroutine
+
+from keys import KEYS
+from texts import twilio_client, BASE_URL
+from . import database as db
+from . import prompts
 
 
 class CallTool(BaseTool):
@@ -16,7 +23,37 @@ class CallTool(BaseTool):
     )
 
     def _run(self, query: str) -> str:
-        pass
+        """Make a call."""
+        try:
+            input_parsed = json.loads(query)
+        except Exception as e:
+            return f"Error parsing input: {str(e)}"
+
+        if not "recipient_phone" in input_parsed:
+            return "Input must have a \"recipient_phone\" key."
+        
+        if not "goal" in input_parsed:
+            return "Input must have a \"goal\" key."
+
+        recipient = str(input_parsed["recipient_phone"])
+        goal = str(input_parsed["goal"])
+
+        params = {
+            "goal": goal,
+            "greeting_id": db.encode_greeting(prompts.generate_intro_message(goal))
+        }
+
+        outbound_call = twilio_client.calls.create(
+            recipient,
+            KEYS["Twilio"]["sender"],
+            url=f"{BASE_URL}/voice/outbound/handler?{urlencode(params)}"
+        )
+
+        # Wait for call to complete
+        while outbound_call.update().status != "completed":
+            pass
+
+        return "Call completed."
 
     def _arun(self, *args: Any, **kwargs: Any) -> Coroutine[Any, Any, str]:
         raise NotImplementedError()
