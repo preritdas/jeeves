3ba5c08f4b4106e31c4f71e0a064ab479afcf576
Prerit Das <preritdas@gmail.com>
2023-04-26 00:01:41 +0000

    Consolidate all into convo, no greetings.

--- apps/gpt/make_calls/database.py
+++ apps/gpt/make_calls/database.py
@@ -7,38 +7,37 @@ import voice_tools as vt
 
 deta_client = deta.Deta(KEYS["Deta"]["project_key"])
 convo_base = deta_client.Base("conversations")
-greetings_base = deta_client.Base("greetings")
 
 
 def create_call(goal: str, greeting: str) -> str:
     """Creates a call and returns a call ID."""
-    key = convo_base.put({"convo": ""})["key"]
-    greetings_base.put(
+    conversation = convo_base.put(
         {
-            "goal": goal, 
+            "convo": "",
+            "goal": goal,
             "greeting": greeting,
             "greeting_url": vt.speak.speak_jeeves(greeting)
-        },
-        key=key
+        }
     )
-    return key
+
+    return conversation["key"]
 
 
 def decode_greeting(call_id: str) -> str:
     """Uses ID to find greeting."""
-    res = greetings_base.get(call_id)["greeting"]
+    res = convo_base.get(call_id)["greeting"]
     return res
 
 
 def decode_greeting_url(call_id: str) -> str:
     """Uses ID to find greeting."""
-    res = greetings_base.get(call_id)["greeting_url"]
+    res = convo_base.get(call_id)["greeting_url"]
     return res
 
 
 def decode_goal(call_id: str) -> str:
     """Uses ID to find goal."""
-    res = greetings_base.get(call_id)["goal"]
+    res = convo_base.get(call_id)["goal"]
     return res
 
 
