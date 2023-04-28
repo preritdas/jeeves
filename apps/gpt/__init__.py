07a9f77724cf13b21b07e4ff4fbe1b991db66264
Prerit Das <preritdas@gmail.com>
2023-04-28 02:02:05 +0000

    Better help and agency option parsing.

--- apps/gpt/__init__.py
+++ apps/gpt/__init__.py
@@ -6,13 +6,16 @@ from . import tool_auth
 from . import completions
 
 
-APP_HELP = "Get a GPT response."
+APP_HELP = "Speak with Jeeves."
+OPTIONS = {
+    "agency": "'no' if you want a basic GPT response, not Jeeves."
+}
 
 
-@utils.app_handler(APP_HELP)
+@utils.app_handler(APP_HELP, OPTIONS)
 def handler(content: str, options: dict[str, str]) -> str:
     """Handler for the GPT applet."""
-    if options.get("agency") == "no":
+    if options.get("agency").lower() in {"no", "false", "off"}:
         return completions.gpt_response(content)
 
     toolkit = tool_auth.build_tools(options["inbound_phone"])
