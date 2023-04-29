7d639c1ab121bd9d31cea5f5fcac9f7adc53ee6e
Prerit Das <preritdas@gmail.com>
2023-04-29 03:21:06 +0000

    Call class so each route has to only call Deta API once.

--- apps/gpt/make_calls/database.py
+++ apps/gpt/make_calls/database.py
@@ -9,6 +9,41 @@ deta_client = deta.Deta(KEYS["Deta"]["project_key"])
 convo_base = deta_client.Base("conversations")
 
 
+class Call:
+    """Contains all the relevant information in a call."""
+    def __init__(
+        self, 
+        key: str,
+        convo: str, 
+        goal: str, 
+        recipient_desc: str, 
+        greeting: str, 
+        greeting_url: str
+    ) -> None:
+        self.key = key
+        self.convo = convo
+        self.goal = goal
+        self.recipient_desc = recipient_desc
+        self.greeting = greeting
+        self.greeting_url = greeting_url
+
+    def update(self) -> None:
+        """Update the call record in Deta."""
+        updates = self.__dict__.copy()
+        del updates["key"]
+
+        convo_base.update(
+            key=self.key,
+            updates=updates
+        )
+
+    @classmethod
+    def from_call_id(cls, call_id: str) -> "Call":
+        """Initialize a call object using just a call id."""
+        call = convo_base.get(call_id)
+        return cls(**call)
+
+
 def create_call(goal: str, greeting: str, recipient_desc: str) -> str:
     """Creates a call and returns a call ID."""
     conversation = convo_base.put(
