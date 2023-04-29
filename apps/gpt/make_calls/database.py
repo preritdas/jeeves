6dff3d009b741ea6b58debf279d573b25b6cfd19
Prerit Das <preritdas@gmail.com>
2023-04-29 03:01:02 +0000

    Space for recipient description.

--- apps/gpt/make_calls/database.py
+++ apps/gpt/make_calls/database.py
@@ -9,12 +9,13 @@ deta_client = deta.Deta(KEYS["Deta"]["project_key"])
 convo_base = deta_client.Base("conversations")
 
 
-def create_call(goal: str, greeting: str) -> str:
+def create_call(goal: str, greeting: str, recipient_desc: str) -> str:
     """Creates a call and returns a call ID."""
     conversation = convo_base.put(
         {
             "convo": "",
             "goal": goal,
+            "recipient_desc": recipient_desc,
             "greeting": greeting,
             "greeting_url": vt.speak.speak_jeeves(greeting)
         }
@@ -38,6 +39,12 @@ def decode_greeting_url(call_id: str) -> str:
 def decode_goal(call_id: str) -> str:
     """Uses ID to find goal."""
     res = convo_base.get(call_id)["goal"]
+    return res
+
+
+def decode_recipient_desc(call_id: str) -> str:
+    """Uses ID to find recipient description."""
+    res = convo_base.get(call_id)["recipient_desc"]
     return res
 
 
