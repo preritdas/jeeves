b5f260ec93a33f8dd2d49c3c95295253a7bd9fb5
Prerit Das <preritdas@gmail.com>
2023-05-01 06:59:47 +0000

    Rename and handle all log stuff.

--- apps/gpt/logs_callback.py
+++ apps/gpt/logs_callback.py
@@ -1,10 +1,15 @@
 """Create a logging callback handler for the agent."""
 from langchain.callbacks.base import BaseCallbackHandler
+from langchain.callbacks import CallbackManager
 
+import logging
 from logging import Logger
+from logging.handlers import SysLogHandler
 
 from typing import Dict, Any, List, Optional, Union
 from langchain.schema import LLMResult, AgentAction, AgentFinish
+
+from keys import KEYS
 
 
 class AgentLoggingCallbackHandler(BaseCallbackHandler):
@@ -42,6 +47,7 @@ class AgentLoggingCallbackHandler(BaseCallbackHandler):
         """Print out that we are entering a chain."""
         class_name = serialized["name"]
         self.logger.info(f"AgentStart: Entering new {class_name} chain...")
+        self.logger.info(f"UserInput: {inputs['input']}")
 
     def on_chain_end(self, outputs: Dict[str, Any], **kwargs: Any) -> None:
         """Print out that we finished a chain."""
@@ -60,13 +66,12 @@ class AgentLoggingCallbackHandler(BaseCallbackHandler):
         **kwargs: Any,
     ) -> None:
         """Do nothing."""
-        pass
 
     def on_agent_action(
         self, action: AgentAction, color: Optional[str] = None, **kwargs: Any
     ) -> Any:
         """Run on agent action."""
-        self.logger.info(action.log)
+        self.logger.info(f"AgentAction: {action.tool}: {action.tool_input}")
 
     def on_tool_end(
         self,
@@ -77,13 +82,7 @@ class AgentLoggingCallbackHandler(BaseCallbackHandler):
         **kwargs: Any,
     ) -> None:
         """If not the final action, print out observation."""
-        if observation_prefix is not None:
-            self.logger.info(observation_prefix)
-
-        self.logger.info(output)
-
-        if llm_prefix is not None:
-            self.logger.info(llm_prefix)
+        self.logger.info(f"{observation_prefix}{output}")
 
     def on_tool_error(
         self, error: Union[Exception, KeyboardInterrupt], **kwargs: Any
@@ -106,3 +105,15 @@ class AgentLoggingCallbackHandler(BaseCallbackHandler):
     ) -> None:
         """Run on agent end."""
         self.logger.info(f"FinalAnswer: {finish.return_values['output']}")
+
+
+# ---- Logging ----
+
+logger = logging.getLogger("agent")
+logger.setLevel(logging.INFO)
+handler = SysLogHandler(address=(KEYS.Papertrail.host, KEYS.Papertrail.port))
+logger.addHandler(handler)
+
+# Log to console and to Papertrail
+logging_callback = AgentLoggingCallbackHandler(logger=logger)
+callback_manager = CallbackManager([logging_callback])
