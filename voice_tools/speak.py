8cc13fbdfd2463b2284f388692afa35619007071
Prerit Das <preritdas@gmail.com>
2023-05-05 19:57:00 +0000

    Make uploading more flexible.

--- voice_tools/speak.py
+++ voice_tools/speak.py
@@ -2,9 +2,6 @@
 import elevenlabs
 import requests
 
-import os
-import uuid
-
 from keys import KEYS
 from voice_tools import speech_cache
 
@@ -12,18 +9,15 @@ from voice_tools import speech_cache
 JEEVES_VOICE_ID = KEYS.ElevenLabs.voice_id
 
 
-def _upload_result(filepath: str) -> str:
+def _upload_result(bytecode: bytes, mime: str) -> str:
     """Upload the result to the server and return the URL."""
     url = f"https://api.upload.io/v2/accounts/{KEYS.UploadIO.account}/uploads/binary"
     headers = {
         "Authorization": f"Bearer {KEYS.UploadIO.api_key}",
-        "Content-Type": "audio/mpeg"
+        "Content-Type": mime
     }
 
-    with open(filepath, "rb") as f:
-        data = f.read()
-        response = requests.post(url, headers=headers, data=data)
-
+    response = requests.post(url, headers=headers, data=bytecode)
     return response.json()["fileUrl"]
 
 
@@ -34,13 +28,7 @@ def speak_jeeves(text: str) -> str:
         return cached_url
 
     byte_code = elevenlabs.generate(text, KEYS.ElevenLabs.api_key, JEEVES_VOICE_ID)
-    filepath = f"{uuid.uuid1()}.mp3"
-
-    with open(filepath, "wb") as f:
-        f.write(byte_code)
-
-    public_url = _upload_result(filepath)
-    os.remove(filepath)
+    public_url = _upload_result(byte_code, "audio/mpeg")
 
     # Cache the speech
     speech_cache.cache_speech(text, public_url)
