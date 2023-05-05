3a67e5d6ed916cf27297aff29ef4bda915c442a9
Prerit Das <preritdas@gmail.com>
2023-05-05 20:30:12 +0000

    Support not just MP3.
    Kind of ugly, please refactor.

--- voice_tools/speak.py
+++ voice_tools/speak.py
@@ -1,6 +1,10 @@
 """Use ElevenLabs to speak."""
 import elevenlabs
 import requests
+from pydub import AudioSegment
+
+import io
+import uuid
 
 from keys import KEYS
 from voice_tools import speech_cache
@@ -9,7 +13,7 @@ from voice_tools import speech_cache
 JEEVES_VOICE_ID = KEYS.ElevenLabs.voice_id
 
 
-def _upload_result(bytecode: bytes, mime: str) -> str:
+def _upload_result(bytecode: bytes, filetype: str, mime: str) -> str:
     """Upload the result to the server and return the URL."""
     url = f"https://api.upload.io/v2/accounts/{KEYS.UploadIO.account}/uploads/binary"
     headers = {
@@ -17,20 +21,55 @@ def _upload_result(bytecode: bytes, mime: str) -> str:
         "Content-Type": mime
     }
 
-    response = requests.post(url, headers=headers, data=bytecode)
+    # Create a filename
+    filename = str(uuid.uuid1()) + f".{filetype}"
+
+    response = requests.post(
+        url, 
+        headers=headers, 
+        data=bytecode, 
+        params={"fileName": filename}
+    )
+
     return response.json()["fileUrl"]
 
 
-def speak_jeeves(text: str) -> str:
-    """Speak the text using the Jeeves voice. Returns a public URL path."""
+def _speak_jeeves(text: str) -> bytes:
+    """Speak the text using the Jeeves voice. Returns MP3 bytecode."""
+    return elevenlabs.generate(text, KEYS.ElevenLabs.api_key, JEEVES_VOICE_ID)
+
+
+def speak_jeeves(
+    text: str, 
+    output_format: str = "mp3", 
+    output_mime: str = "audio/mpeg"
+) -> str:
+    """
+    Speak the text using the Jeeves voice. Returns a public URL path.
+    Caching only happens for MP3 files. If changing the output format
+    or mime type, be sure to change *both* arguments so they match. 
+    Otherwise, weird things may happen.
+    """
     # Check if the speech is cached
-    if (cached_url := speech_cache.get_speech(text)):
+    if output_mime == "audio/mpeg" and (cached_url := speech_cache.get_speech(text)):
         return cached_url
 
-    byte_code = elevenlabs.generate(text, KEYS.ElevenLabs.api_key, JEEVES_VOICE_ID)
-    public_url = _upload_result(byte_code, "audio/mpeg")
+    # Generate MP3 bytecode
+    byte_code = _speak_jeeves(text)
 
-    # Cache the speech
-    speech_cache.cache_speech(text, public_url)
+    # Convert if necessary
+    if output_mime != "audio/mpeg":
+        segment = AudioSegment.from_mp3(io.BytesIO(byte_code))
+        result_stream = io.BytesIO()
+        segment.export(result_stream, format=output_format, codec="libopus")
+        result_stream.seek(0)
+        byte_code = result_stream.getvalue()
+
+    # Upload for public link with UploadIO
+    public_url = _upload_result(byte_code, output_format, output_mime)
+
+    # Cache the speech but only for MP3
+    if output_mime == "audio/mpeg":
+        speech_cache.cache_speech(text, public_url)
 
     return public_url
