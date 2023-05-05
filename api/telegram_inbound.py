2e186a757ce6b92edae9aa618433eb5146f7d12b
Prerit Das <preritdas@gmail.com>
2023-05-05 19:46:40 +0000

    Respond to voice messages on Telegram.

--- api/telegram_inbound.py
+++ api/telegram_inbound.py
@@ -4,6 +4,7 @@ import requests
 from keys import KEYS
 
 from apps.gpt import generate_agent_response
+from voice_tools.transcribe import transcribe_telegram_file_id
 
 
 router = APIRouter()
@@ -30,7 +31,14 @@ async def send_message(user_id: int, message: str):
 async def handle_inbound_telegram(request: Request) -> str:
     req = await request.json()
     inbound_id = int(req["message"]["from"]["id"])
-    inbound_body = req["message"]["text"]
+
+    # Get the inbound body
+    if "text" in req["message"]:
+        inbound_body = req["message"]["text"]
+    elif "voice" in req["message"]:
+        inbound_body = transcribe_telegram_file_id(req["message"]["voice"]["file_id"])
+    else:
+        await send_message(inbound_id, "I'm sorry, sir, but I don't understand that yet.")
 
     # Try to get the phone number from the inbound ID
     recognized_user: str = KEYS.Telegram.id_phone_mapping.get(inbound_id, "")
