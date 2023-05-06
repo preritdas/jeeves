3653406ef7c695584bef5a0f397d6ae0c20b5654
Prerit Das <preritdas@gmail.com>
2023-05-06 21:18:41 +0000

    Allow isntantiation with string datetime.

--- apps/gpt/user_memory/models.py
+++ apps/gpt/user_memory/models.py
@@ -1,5 +1,5 @@
 """Models for the user memory tool."""
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
 
 import datetime as dt
 
@@ -9,6 +9,12 @@ class Entry(BaseModel):
     datetime: dt.datetime
     user_phone: str
     content: str
+
+    @validator("datetime", pre=True)
+    def parse_datetime(cls, v):
+        """Parse datetime."""
+        if isinstance(v, str):
+            return dt.datetime.fromisoformat(v)
 
     def to_dict(self) -> dict:
         """Return the entry as a dictionary."""
