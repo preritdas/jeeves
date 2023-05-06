7cd9df95d35fb9bdcaccf4826a2baf41ac48f26a
Prerit Das <preritdas@gmail.com>
2023-05-06 22:43:40 +0000

    Test building user memory tools.

--- tests/test_user_memory.py
+++ tests/test_user_memory.py
@@ -1,4 +1,6 @@
 """Test user memory."""
+from langchain.tools import BaseTool
+
 from apps.gpt.user_memory import UserMemory, create_user_memory_tools
 
 
@@ -16,3 +18,14 @@ def test_retrieving_from_memory(temporary_user_memory):
     """
     user_memory = UserMemory.from_user_phone(temporary_user_memory)
     assert "2" in user_memory.answer_question("Where did I park?")
+
+
+def test_building_user_memory_tools(temporary_user_memory):
+    """
+    Test building user memory tools. 
+    This tests prompts and prompt reading and building the tools.
+    """
+    user_memory_tools = create_user_memory_tools(temporary_user_memory)
+
+    assert isinstance(user_memory_tools, list)
+    assert all(isinstance(tool, BaseTool) for tool in user_memory_tools)
