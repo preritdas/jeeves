a8f501635df78844762bf4ce43280e8731eab723
Prerit Das <preritdas@gmail.com>
2023-05-06 20:24:24 +0000

    Test validating keys models.

---
+++ tests/test_keys.py
@@ -0,0 +1,6 @@
+"""Test keys models."""
+
+def test_keys_models():
+    """Test that the keys models are valid."""
+    # This will raise an exception if the models are invalid.
+    from keys import KEYS
