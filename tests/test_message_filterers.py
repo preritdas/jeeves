5b17073e8a962a2fa0a3ffc7f5423560d0a965f8
Prerit Das <preritdas@gmail.com>
2023-05-07 19:09:06 +0000

    Tests for DatetimeFilterer.

--- tests/test_message_filterers.py
+++ tests/test_message_filterers.py
@@ -5,7 +5,7 @@ import datetime as dt
 import pytz
 
 from config import CONFIG
-from apps.gpt.chat_history.filter import RecencyFilterer
+from apps.gpt.chat_history.filter import RecencyFilterer, DatetimeFilterer
 from apps.gpt.chat_history.models import Message
 
 
@@ -54,6 +54,8 @@ def temporary_messages(default_inbound) -> list[Message]:
     ]
 
 
+# ---- Recency ----
+
 def test_recency_filterer(temporary_messages):
     """
     Test the recency filterer. Make sure it returns the correct number of messages,
@@ -76,3 +78,43 @@ def test_recency_filterer(temporary_messages):
     # Make sure the messages are in chronological order
     assert filtered_messages == sorted(filtered_messages, key=lambda m: m.datetime)
     
+
+# ---- Datetime ----
+
+def test_datetime_filterer(temporary_messages):
+    """
+    Test the datetime filterer. Make sure it returns the correct messages that fall
+    within the specified datetime range.
+    """
+    start_datetime = dt.datetime(2021, 1, 2, 0, 0, 0, tzinfo=tz)
+    end_datetime = dt.datetime(2021, 1, 5, 0, 0, 0, tzinfo=tz)
+    filterer = DatetimeFilterer(start=start_datetime, end=end_datetime)
+    filtered_messages = filterer.filter_messages(temporary_messages)
+
+    # Make sure 4 messages are within the specified range
+    assert len(filtered_messages) == 4
+
+    # Make sure the messages are in the specified range
+    for message in filtered_messages:
+        assert start_datetime <= message.datetime <= end_datetime
+
+    # Make sure the bookend messages are correct (first and last in the range)
+    assert filtered_messages[0].user_input == "How are you?"
+    assert filtered_messages[-1].user_input == "What's your favorite food?"
+
+    # Messages outside the specified range shouldn't be in the filtered messages
+    assert all(m.user_input != "Hello" for m in filtered_messages)
+    assert all(m.user_input != "What's your favorite animal?" for m in filtered_messages)
+
+
+def test_datetime_filterer_empty_result(temporary_messages):
+    """
+    Test the datetime filterer when no messages fall within the specified datetime range.
+    """
+    start_datetime = dt.datetime(2021, 1, 10, 0, 0, 0, tzinfo=tz)
+    end_datetime = dt.datetime(2021, 1, 12, 0, 0, 0, tzinfo=tz)
+    filterer = DatetimeFilterer(start=start_datetime, end=end_datetime)
+    filtered_messages = filterer.filter_messages(temporary_messages)
+
+    # Make sure no messages are within the specified range
+    assert len(filtered_messages) == 0
