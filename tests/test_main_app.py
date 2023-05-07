b36df7dbbbde15c0ff5b23244c200e3606e6eeef
Prerit Das <preritdas@gmail.com>
2023-05-07 03:38:54 +0000

    Working tests for text inbound routes.

--- tests/test_main_app.py
+++ tests/test_main_app.py
@@ -9,37 +9,30 @@ def test_client():
     return TestClient(app=api.app)
 
 
-@pytest.mark.skip
 def test_testing_endpoint(test_client):
     res = test_client.get("/")
-
-    assert "All working here" in res.text
     assert res.status_code == 200
 
 
-@pytest.mark.skip
 def test_apps_non_threaded(test_client, default_inbound, mocker):
-    mocker.patch("main.inbound.texts.CONFIG.General.sandbox_mode", True)
-    mocker.patch("main.inbound.usage.CONFIG.General.sandbox_mode", True)
+    mocker.patch("api.text_inbound.inbound.texts.CONFIG.General.sandbox_mode", True)
+    mocker.patch("api.text_inbound.inbound.usage.CONFIG.General.sandbox_mode", True)
+    mocker.patch("api.text_inbound.CONFIG.General.threaded_inbound", False)
 
-    mocker.patch("main.CONFIG.General.threaded_inbound", False)
-
-    json = {**default_inbound}
-    del json["concat"]
-
-    res = test_client.post("/inbound-sms", json=json)
+    res = test_client.post(
+        "/texts/inbound-sms",
+        data={"From": default_inbound["phone_number"], "Body": default_inbound["body"]},
+    )
     assert res.status_code == 204
 
 
-@pytest.mark.skip
 def test_apps_threaded(test_client, default_inbound, mocker):
-    mocker.patch("main.inbound.texts.CONFIG.General.sandbox_mode", True)
-    mocker.patch("main.inbound.usage.CONFIG.General.sandbox_mode", True)
+    mocker.patch("api.text_inbound.inbound.texts.CONFIG.General.sandbox_mode", True)
+    mocker.patch("api.text_inbound.inbound.usage.CONFIG.General.sandbox_mode", True)
+    mocker.patch("api.text_inbound.CONFIG.General.threaded_inbound", True)
 
-    mocker.patch("main.CONFIG.General.threaded_inbound", True)
-
-    json = {**default_inbound}
-    del json["concat"]
-
-    res = test_client.post("/inbound-sms", json=json)
+    res = test_client.post(
+        "/texts/inbound-sms",
+        data={"From": default_inbound["phone_number"], "Body": default_inbound["body"]},
+    )
     assert res.status_code == 204
