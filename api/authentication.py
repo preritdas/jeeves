2aeac8327dcd44c7c4d571683d36198bfcb0cf8a
Prerit Das <preritdas@gmail.com>
2023-05-15 23:59:10 +0000

    Redirect with URI.

--- api/authentication.py
+++ api/authentication.py
@@ -4,6 +4,7 @@ from fastapi.responses import RedirectResponse
 import requests
 from deta import Deta
 
+from jeeves.texts import BASE_URL
 from jeeves.keys import KEYS
 
 
@@ -16,9 +17,12 @@ permissions_db = Deta(KEYS.Deta.project_key).Base("permissions")
 @router.get("/zapier-start/{user_key}")
 async def zapier_start(user_key: str) -> str:
     """Create the link for the user to use to start with Zapier."""
+    redirect_uri = BASE_URL + f"/auth/zapier-handler/{user_key}"
+
     return RedirectResponse(
         f"https://nla.zapier.com/oauth/authorize?"
         f"client_id={KEYS.Zapier.client_id}&response_type=code"
+        f"&redirect_uri={redirect_uri}"
         "&scope=nla%3Aexposed_actions%3Aexecute"
     )
 
