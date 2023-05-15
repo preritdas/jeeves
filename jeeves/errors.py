99135ef9a41c20387827a90dfdb64a41a62691c6
Prerit Das <preritdas@gmail.com>
2023-05-15 20:33:52 +0000

    Errors.

--- jeeves/errors.py
+++ jeeves/errors.py
@@ -1,2 +1,14 @@
+"""Errors used throughout."""
+
 class InvalidInbound(Exception):
-    pass
+    """
+    Raised when an inbound request is invalid, either because it's missing a required
+    field or because the request is malformed.
+    """
+
+
+class ZapierAuthenticationError(Exception):
+    """
+    Raised when Zapier authentication fails, either on the side of checking access 
+    token validity, refreshing access tokens, or onboarding a user initially.
+    """
