129d4ebf9485637e70ca8fb9d04791b165d67538
Prerit Das <preritdas@gmail.com>
2023-05-16 20:11:46 +0000

    Redirect with `state` not `user_key`.

--- api/authentication.py
+++ api/authentication.py
@@ -19,29 +19,30 @@ permissions_db = Deta(KEYS.Deta.project_key).Base("permissions")
 @router.get("/zapier-start/{user_key}")
 async def zapier_start(user_key: str) -> str:
     """Create the link for the user to use to start with Zapier."""
-    redirect_uri = BASE_URL + f"/auth/zapier-handler/{user_key}"
+    redirect_uri = BASE_URL + "/auth/zapier-handler/"
     redirect_uri = urllib.parse.quote_plus(redirect_uri)
 
     return RedirectResponse(
-        f"https://nla.zapier.com/oauth/authorize?"
-        f"client_id={KEYS.Zapier.client_id}&response_type=code"
+        f"https://nla.zapier.com/oauth/authorize/"
+        f"&client_id={KEYS.Zapier.client_id}&response_type=code"
         f"&redirect_uri={redirect_uri}"
+        f"&state={user_key}"
         "&scope=nla%3Aexposed_actions%3Aexecute"
     )
 
 
-@router.get("/zapier-handler/{user_key}")
-def handle_zapier(user_key: str, code: str):
+@router.get("/zapier-handler")
+def handle_zapier(state: str, code: str):
     """
     Handle a Zapier authentication code. Takes the code and update's the user's
     access token in the permissions database.
 
     Args:
-        user_key (str): The user key - Deta key for their user entry.
+        state (str): The user key - Deta key for their user entry.
         code (str): The authentication code.
     """
     # Find the user in the database
-    user = permissions_db.get(user_key)
+    user = permissions_db.get(state)
 
     if not user:
         return {"error": "User not found."}
