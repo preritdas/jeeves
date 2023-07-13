ddfa7559f7519bbd4ad1c66674f376230401c713
Prerit Das <preritdas@gmail.com>
2023-07-12 21:22:40 -0400

    Apply test uid to base agent tests.

--- tests/agency/test_base_agent.py
+++ tests/agency/test_base_agent.py
@@ -2,10 +2,14 @@
 from jeeves.agency import generate_base_agent_response
 
 
-def test_base_agent():
+def test_base_agent(mocker, callback_uid):
+    """Test getting any response from the base agent."""
+    mocker.patch("jeeves.agency.uuid.uuid4", return_value=callback_uid)
     assert generate_base_agent_response("Hi.")
 
 
-def test_search():
+def test_search(mocker, callback_uid):
+    """Test using a single generic tool."""
+    mocker.patch("jeeves.agency.uuid.uuid4", return_value=callback_uid)
     assert generate_base_agent_response("weather in washington dc")
-  
+ 
